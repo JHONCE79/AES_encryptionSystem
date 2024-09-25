1c9cf635d90337dc05ed440d89e76b4e426e3c1b
Santiago <140856574+Stavera21@users.noreply.github.com>
2024-09-25 15:20:37 -0500

    Add files via upload

---
+++ src/Console/console.py
@@ -0,0 +1,30 @@
+from AES import *
+class Console:
+    def main(self) -> None:
+        print("Menú de AES:")
+        print("1. Encriptar mensaje")
+        print("2. Desencriptar mensaje")
+        print("3. Salir")
+
+        while True:
+            opcion = input("Ingrese una opción: ")
+
+            if opcion == "1":
+                clave = input("Ingrese la clave de encriptación: ")
+                mensaje = input("Ingrese el mensaje a encriptar: ")
+                encriptado = encrypt(clave, mensaje)
+                print("Mensaje encriptado:", encriptado.hex())
+            elif opcion == "2":
+                clave = input("Ingrese la clave de desencriptación: ")
+                mensaje_encriptado = input("Ingrese el mensaje encriptado (en hexadecimal): ")
+                mensaje_encriptado = bytes.fromhex(mensaje_encriptado)
+                desencriptado = decrypt(clave, mensaje_encriptado)
+                print("Mensaje desencriptado:", desencriptado.decode("utf-8"))
+            elif opcion == "3":
+                print("Adiós!")
+                break
+            else:
+                print("Opción inválida. Intente de nuevo.")
+
+    if __name__ == "__main__":
+        main('self')
